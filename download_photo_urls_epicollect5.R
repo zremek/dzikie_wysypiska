32c4da0ea39c661803da14df5fe4cbefbe7b920a
Remigiusz Żulicki <remigiuszzulicki@gmail.com>
2022-04-29 13:47:44 +0200

    update photo download to include more fields, some plot fix for less manual work

--- download_photo_urls_epicollect5.R
+++ download_photo_urls_epicollect5.R
@@ -1,17 +1,23 @@
 library(jsonlite)
 
-l <- jsonlite::fromJSON(txt = "form-1-dzikie-wysypiska-4.json",
-              flatten = TRUE)
+# step 0: manually login and download json file #####
+# from https://five.epicollect.net/project/dzikie-wysypiska/data 
+
+# 1: make df with photo urls ####
+
+my_fresh_file <- "2022-04-29-form-1__dzikie-wysypiska.json"
+
+l <- jsonlite::fromJSON(txt = my_fresh_file,
+                        flatten = TRUE)
 
 d <- l[["data"]]
 
-## photos here:
-# 1_Prosimy_o_zdjcie_z
-# 2_Jeli_chcesz_dodaj_ 
-
 d1 <- d[d$`1_Prosimy_o_zdjcie_z` != "",]
 d2 <- d[d$`2_Jeli_chcesz_dodaj_` != "",]
+d3 <- d[d$`3_Jeli_chcesz_dodaj_` != "",]
+d4 <- d[d$`4_Jeli_chcesz_dodaj_` != "",]
 
+# 2: loop for download #### 
 # https://stackoverflow.com/questions/32174306/download-url-links-using-r 
 
 for (i in 1:length(d1$`1_Prosimy_o_zdjcie_z`)) {
@@ -23,5 +29,17 @@ for (i in 1:length(d1$`1_Prosimy_o_zdjcie_z`)) {
 for (i in 1:length(d2$`2_Jeli_chcesz_dodaj_`)) {
   download.file(d2$`2_Jeli_chcesz_dodaj_`[i],
                 destfile = paste0("photos/d2_", d2$ec5_uuid[i], ".jpg"),
+                method = "auto")
+}
+
+for (i in 1:length(d3$`3_Jeli_chcesz_dodaj_`)) {
+  download.file(d3$`3_Jeli_chcesz_dodaj_`[i],
+                destfile = paste0("photos/d3_", d3$ec5_uuid[i], ".jpg"),
+                method = "auto")
+}
+
+for (i in 1:length(d4$`4_Jeli_chcesz_dodaj_`)) {
+  download.file(d4$`4_Jeli_chcesz_dodaj_`[i],
+                destfile = paste0("photos/d4_", d4$ec5_uuid[i], ".jpg"),
                 method = "auto")
 }
--- wykres_liczba_wpisow_dzikie.R
+++ wykres_liczba_wpisow_dzikie.R
@@ -1,7 +1,7 @@
 library(jsonlite)
 library(tidyverse)
 
-data_json <- "2022-04-12-form-1__dzikie-wysypiska.json"
+data_json <- "2022-04-29-form-1__dzikie-wysypiska.json"
 
 l <- jsonlite::fromJSON(txt = data_json,
                         flatten = TRUE)
@@ -14,14 +14,17 @@ d <- d %>% mutate(date_user = parse_date(x = `7_Dzisiejsza_data__p`, format = "%
 
 summary(d$date_user)
 
-# TODO dodać to do wykresu paste() #####
-# d %>% count(date_user) %>% pull(n) %>% sum() 
+# plot #####
 
+count_date_user <- d %>% count(date_user)
+n_max <- count_date_user %>% pull(n) %>% max()
+n_sum <- count_date_user %>% pull(n) %>% sum()
+date_max <- max(d$date_user)
 
-d %>% count(date_user) %>% ggplot(aes(x = date_user, y = n)) +
+count_date_user %>% ggplot(aes(x = date_user, y = n)) +
   geom_col(width = 0.1, fill = "seagreen") +
   geom_point(colour = "seagreen", size = 3) + 
-  scale_y_continuous(breaks = 0:6, minor_breaks = NULL, limits = c(0, 6),
+  scale_y_continuous(breaks = 0:n_max, minor_breaks = NULL, limits = c(0, n_max),
                      name = "liczba wpisów") + 
   scale_x_date(date_minor_breaks = "1 day",
                name = "data dodania wpisu",
@@ -29,14 +32,18 @@ d %>% count(date_user) %>% ggplot(aes(x = date_user, y = n)) +
                date_labels = "%d.%m") + 
   theme_minimal() + 
   labs(title = "Wpisy na łódzkiej mapie dzikich wysypisk", 
-       subtitle = "Od 1. marca do 11. kwietnia 2022 przesłaliście nam 95 wpisów, dziękujemy!", 
+       subtitle = paste0("Do ", 
+                         date_max, 
+                         " przesłaliście nam ",
+                         n_sum, 
+                         " wpisy, dziękujemy!"), 
        caption = "Źródło: badania własne\ndzikiewysypiska.uni.lodz.pl")
 
 # ggsave(filename = paste(Sys.Date(), "liczba_wpisow_E5.png"),
 #        units = "cm", height = 5, width = 7) źle działa!!!
 
-ggsave(filename = paste(Sys.Date(), "_liczba_wpisow_E5.jpg", sep = ""),
-       scale = 1, units = "in", width = 6, height = 4)
+ggsave(filename = paste(date_max, "_liczba_wpisow_E5.jpg", sep = ""),
+       scale = 1, units = "in", width = 8, height = 4)
 
 
 # ile zdjęć w sumie? #####
@@ -48,6 +55,5 @@ d %>% select(5:8) %>%
 # jaki czas odpowiedzi? 
 
 d %>% mutate(czas_odp_sekundy = as.numeric(
-  parse_datetime(uploaded_at) - parse_datetime(created_at))
-) %>% 
+  parse_datetime(uploaded_at) - parse_datetime(created_at))) %>% 
   pull(czas_odp_sekundy) %>% summary()
