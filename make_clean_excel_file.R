00ffe3f076b9a05095a513c2817fab2e0d8b99ff
Remigiusz Żulicki <remigiuszzulicki@gmail.com>
2022-11-17 16:46:09 +0100

    done data cleaning

--- make_clean_excel_file.R
+++ make_clean_excel_file.R
@@ -4,4 +4,78 @@ Sys.setenv(LANGUAGE = "en")
 
 d_clean <- read_rds("d_clean.rds")
 
+## w osobnej ramce słownik z etykietami pol / eng 
+## to idzie jako kolejna karta w excelu 
 
+e5_data_dictionary_pl <- tibble(var_names = names(d_clean))
+
+e5_data_dictionary_pl$var_labels_pl <- NA
+
+e5_data_dictionary_pl$var_labels_pl[1:21] <- c(
+  "Identyfikator techniczny wpisu w E5",
+  "Data i godzina zamknięcia formularza (UTC)",
+  "Data i godzina przesłania formularza (UTC)",
+  "Identyfikator czytelny wpisu w E5 (data i godzina wskazana przez użytkownika, nazwa użytkownika jeżeli pierwszy wpis)",
+  "Zdjęcie znalezionego dzikiego wysypiska (1)", 
+  "Zdjęcie znalezionego dzikiego wysypiska (2)",
+  "Zdjęcie znalezionego dzikiego wysypiska (3)",
+  "Zdjęcie znalezionego dzikiego wysypiska (4)",
+  "Tutaj zlokalizowane jest dzikie wysypisko (szerokość geogr. wskazana przez użytkownika)",
+  "Tutaj zlokalizowane jest dzikie wysypisko (długość geogr. wskazana przez użytkownika)",
+  "Jeżeli masz uwagi do lokalizacji znalezionego dzikiego wysypiska, prosimy o wpisanie",
+  "Dzisiejsza data - prosimy o potwierdzenie (wskazana przez użytkownika)",
+  "Aktualny czas - prosimy o potwierdzenie (wskazana przez użytkownika)", 
+  "Czy chcesz nam powiedzieć coś więcej o znalezionym wysypisku?",
+  "Jak oceniasz powierzchnię znalezionego dzikiego wysypiska?",
+  "Jaki jest charakter znalezionego wysypiska?",
+  "Opisz charakter znalezionego wysypiska",
+  "Czy możesz ocenić czas istnienia znalezionego wysypiska?",
+  "W jakim miejscu jest znalezione wysypisko?",
+  "Opisz miejsce znalezionego wysypiska",
+  "Czy znalezione wysypisko jest widoczne dla przechodniów?"
+)
+
+t <- d_clean %>% 
+        select(1, 22:40) %>%
+        pivot_longer(!ecuuid) %>%
+        filter(!is.na(value)) %>%
+        group_by(name) %>%
+        count(value) %>% 
+        select(-n) %>% 
+        mutate(value = paste0(value, ": Jakie rodzaje odpadów (materiałów) znajdują się w wysypisku?")) %>% 
+        rename(var_names = name, 
+               var_labels_pl = value)
+
+e5_data_dictionary_pl <- left_join(e5_data_dictionary_pl, t, by = "var_names")
+
+e5_data_dictionary_pl <- 
+  e5_data_dictionary_pl %>%
+  unite("var_labels_pl", var_labels_pl.x, var_labels_pl.y, sep = "", na.rm = TRUE)
+
+
+e5_data_dictionary_pl$var_labels_pl[41:55] <- c(
+  "Opisz rodzaj znalezionych odpadów (materiałów)", 
+  "Czy po raz pierwszy przesyłasz nam jakiekolwiek zdjęcie dzikiego wysypiska?",
+  "Wymyśl swoją nazwę użytkownika (nowo nadana nazwa przy pierwszym wpisie)",
+  "Wpisz swoją nazwę użytkownika (dotychczasowa nazwa przy kolejnych wpisach, wprowadzana ręcznie)",
+  "Czy chcesz powiedzieć nam coś więcej o sobie? (wyświetla link do zewnętrznego formularza LimeSurvey, pytanie zadane tylko przy pierwszym wpisie)",
+  "Dokładność lokalizacji znalezionego dzikiego wysypiska (promień w metrach)",
+  "UTM Northing (szerokość geogr. wskazana przez użytkownika)",
+  "UTM Easting (długość geogr. wskazana przez użytkownika)",
+  "UTM Zone (strefa lokalizacji wskazana przez użytkownika)",
+  "Identyfikator użytkownika (pochodna new_name_user i old_name_user: nowo nadana lub dotychczasowa nazwa użytkownika, ujednolicona)",
+  "Stopnie dziesiętne do 0.000 001 (szerokość geogr. wskazana przez użytkownika)",
+  "Stopnie dziesiętne do 0.000 001 (długość geogr. wskazana przez użytkownika)",
+  "Suma przesłanych przez użytkownika zdjęć dzikiego wysypiska (pochodna: photo_1...photo_4)", 
+  "Suma podanych przez użytkownika rodzajów odpadów znajdujących się w wysypisku (pochodna: type_glass...type_paints_in)", 
+  "Dzielnica m. Łodzi, w której zlokalizowane jest znalezione wysypisko (pochodna: latitude, longitude, jednostki ewidencyjne Głównego Urzędu Geodezji i Kartografii 2019-08-08)"
+)
+
+
+
+write_xlsx(list(d_clean = d_clean, e5_data_dictionary_pl = e5_data_dictionary_pl),
+           "e5_data_clean.xlsx")
+
+# w samym excelu przygotować oglądanie zdjęć w komórkach 
+# https://www.extendoffice.com/documents/excel/4212-excel-insert-image-from-url.html
+# to VBA działa na macu :) 
--- make_clean_rds_data_file.R
+++ make_clean_rds_data_file.R
@@ -1,20 +1,19 @@
 # goal: make clean, user-friendly excel file including photos 
 # one in Polish, one in English
 
+# eng - academic papers
+# pol - book / report for local audience 
+
+# var names in eng only! 
+
 knitr::knit("data_exploration.Rmd")
 
-## zrobione 1. wyczyścić nazwy zmiennych w d, wzorując się na d_cechy i cechy_tak
-
-# summary(d$date_user)
-
-d_clean <- d %>% rename(zdjecie_1 = `1_Prosimy_o_zdjcie_z`,
-                        zdjecie_2 = `2_Jeli_chcesz_dodaj_`, 
-                        zdjecie_3 = `3_Jeli_chcesz_dodaj_`, 
-                        zdjecie_4 = `4_Jeli_chcesz_dodaj_`)
+d_clean <- d %>% rename(photo_1 = `1_Prosimy_o_zdjcie_z`,
+                        photo_2 = `2_Jeli_chcesz_dodaj_`, 
+                        photo_3 = `3_Jeli_chcesz_dodaj_`, 
+                        photo_4 = `4_Jeli_chcesz_dodaj_`)
 
 d_clean <- rename_with(d_clean, ~tolower(str_remove_all(., "\\d+_")))
-
-## 2. ustalić typy danych, pozbyć się listy w środku
 
 d_clean <- d_clean %>% select(-jakie_rodzaje_odp) %>% as_tibble()
 
@@ -22,20 +21,54 @@ d_clean <- type_convert(d_clean)
 
 ## 3. dołączyć wielokrotny wybór rodzaje_long jako wide 
 
-RODZAJE <- pivot_wider(data = rodzaje_long, id_cols = L1, names_from = value, names_prefix = "rodz_")
-RODZAJE <- RODZAJE %>% rename(ecuuid = L1)
+rodzaje_long <- rodzaje_long %>% 
+  mutate(value = str_trim(value))
 
-colnames(RODZAJE) <- 
-  colnames(RODZAJE) %>% 
+rodzaje_eng <- read_csv("rodzaje_eng.csv")
+
+rodzaje_eng <- rodzaje_eng %>% 
+  mutate(value = str_trim(value),
+         val_eng = str_trim(val_eng))
+
+rodzaje_long_en <- left_join(x = rodzaje_long, 
+                             y = rodzaje_eng)
+
+rodzaje_long_en <- rodzaje_long_en %>% select(-value) %>% 
+  rename(value = val_eng)
+
+print(paste0("class of rodzaje_long_en: ", class(rodzaje_long_en)))
+
+rodzaje_wide_pl <- pivot_wider(data = rodzaje_long,
+                               id_cols = L1,
+                               names_from = value,
+                               names_prefix = "rodz_")
+
+rodzaje_wide_pl <- rodzaje_wide_pl %>% rename(ecuuid = L1)
+
+rodzaje_wide_en <- pivot_wider(data = rodzaje_long_en,
+                               id_cols = L1,
+                               names_from = value,
+                               names_prefix = "type_")
+
+rodzaje_wide_en <- rodzaje_wide_en %>% rename(ecuuid = L1)
+
+colnames(rodzaje_wide_en) <- 
+  colnames(rodzaje_wide_en) %>% 
   stringr::str_squish(string = .) %>% 
-  stringr::str_trunc(string = ., width = 18, side = "right", ellipsis = "") %>% 
+  stringr::str_trunc(string = ., width = 15, side = "right", ellipsis = "") %>% 
   gsub("(", "", ., fixed = TRUE) %>%
   gsub(")", "", ., fixed = TRUE) %>% 
   gsub("/", "", ., fixed = TRUE) %>% 
   gsub(",", "", ., fixed = TRUE) %>% 
   gsub(" ", "_", ., fixed = TRUE)
 
-d_clean <- left_join(d_clean, RODZAJE)
+# rodzaje_wide_en stays for eng version
+
+## make eng var names for pl data
+
+colnames(rodzaje_wide_pl) <- colnames(rodzaje_wide_en)
+
+d_clean <- left_join(d_clean, rodzaje_wide_pl)
 
 ## 4. ustalić co jest NA - załatwione w type_convert
 
@@ -52,18 +85,147 @@ d_clean <- d_clean %>% select(1:8, 25:26, 9, 31, 11:19, 35:53, 20:24, 27:30, 32:
 
 d_clean <- 
   d_clean %>% mutate(
-  zdjecia_sum = rowSums(!is.na(select(., starts_with("zdjecie_")))),
-  rodz_sum = rowSums(!is.na(select(., starts_with("rodz_")))),
-  rodz_sum = if_else(condition = czy_chcesz_nam_pow == "Tak",
-                     true = rodz_sum,
+  photo_sum = rowSums(!is.na(select(., starts_with("photo_")))),
+  type_sum = rowSums(!is.na(select(., starts_with("type_")))),
+  type_sum = if_else(condition = czy_chcesz_nam_pow == "Tak",
+                     true = type_sum,
                      false = NA_real_)
   )
 
-## 6. zapisać Rdata
+## zrobić czytelne nazwy zmiennych 
 
-write_rds(d_clean, "d_clean.rds")
+nazwy_zmiana <- names(d_clean)[c(9:11, 13:21, 41:49)] %>% as_tibble()
 
-## 7. wybrać zmienne do zapisu do excela i zapisać excela 
+nowe_nazwy <- c("lct_lat", 
+                "lct_long",
+                "lct_comment", 
+                "time_user", 
+                "more_dump",
+                "area", 
+                "character", 
+                "character_comment", 
+                "span", 
+                "place", 
+                "place_comment",
+                "visible", 
+                "type_comment", 
+                "first_time_user", 
+                "new_name_user", 
+                "old_name_user",
+                "more_you", 
+                "lct_accu",
+                "lct_northing",
+                "lct_easting", 
+                "lct_zone")
+
+nazwy_zmiana$nowe_nazwy <- nowe_nazwy
+
+names(d_clean)[c(9:11, 13:21, 41:49)] <- nowe_nazwy
+
+names(d_clean) %>% tibble() %>% print(n = 100)
+
+## dodać dzielnicę
+
+districts <- 
+  punkty_dzielnice %>% as_tibble() %>% select(3, 2, -geometry) %>% 
+  rename(district = dzielnica, 
+         ecuuid = ec5_uuid)
+
+d_clean <- left_join(d_clean, districts)
+
+d_clean <-
+  d_clean %>%
+  mutate(district_tmp = ifelse(is.na(lct_zone),
+                           "brak współrzędnych wpisu",
+                           ifelse(is.na(district), 
+                                  "punkt poza granicami m. Łodzi", 
+                                  district)))
+
+# table(d_clean$district, d_clean$lct_zone, useNA = "always")
+# table(d_clean$district_tmp, d_clean$lct_zone, useNA = "always")
+
+d_clean <- d_clean %>% 
+  select(-district) %>% 
+  rename(district = district_tmp)
+
+## zapisać 6 cyfr dokładności po kropce lat / long ################
+
+d_clean %>% 
+  select(ecuuid, created_at, lct_lat, latitude) %>% 
+  filter(ecuuid == "14a666a7-9c5b-4d5e-83a6-3621d7c199c7") %>% 
+  View()
+
+d %>% 
+  select(ec5_uuid, latitude) %>% 
+  filter(ec5_uuid == "14a666a7-9c5b-4d5e-83a6-3621d7c199c7") %>% 
+  pull(latitude)
+
+# mam 51.78125 (8 znaków, 5 cyfr po kropce) 
+# a w JSON źródłowym 51.781247 (9 znaków, 6 cyfr po kropce)
+
+d_clean %>% mutate(lat_digits = nchar(latitude)) %>% pull(lat_digits) %>% table(useNA = "always")
+
+d %>% mutate(lat_digits = nchar(latitude)) %>% pull(lat_digits) %>% table(useNA = "always")
+
+# w większości przypadków jest jednak 9, rzadko 8, czasem 7,
+
+table(nchar(l$data$`5_Tutaj_zlokalizowan.latitude`)) # to samo!
+
+# to samo czytanie JSONa jest złe?
+# https://stackoverflow.com/questions/36038349/significant-digits-when-converting-json-using-fromjson-in-jsonlite 
+
+# sprawdzę jeszcze print
+
+d %>% 
+  select(ec5_uuid, latitude) %>% 
+  filter(ec5_uuid == "14a666a7-9c5b-4d5e-83a6-3621d7c199c7") %>% 
+  pull(latitude) %>% 
+  print(digits = 15)
+# wszystko gra, to tylko kwestia wyświetlania... właśnie się czegoś nauczyłem!
+
+####
+
+# l_digits <- jsonlite::fromJSON(txt = data_json, 
+#                                options(digits = 22, flatten = TRUE)) # to są global options :D
+# 
+# options(digits = 7) # Valid values are 1...22 with default 7.
+
+
+# nchar(l_digits[["data"]][[1]][["5_Tutaj_zlokalizowan"]][["latitude"]])
+
+# nchar daje 8, wypisana liczba to 51.77353000000000093905 ?
+
+
+
+# działa dziwnie, lista wychodzi case by case
+# a wartości lat / lon mają po 
+
+d_clean %>% mutate(lat_digits = nchar(latitude)) %>% 
+  filter(lct_accu < 10000) %>% 
+  ggplot(aes(factor(lat_digits), lct_accu)) + 
+  geom_boxplot()
+
+d_clean %>% mutate(lat_digits = nchar(latitude)) %>% 
+  filter(lct_accu < 1000) %>%
+  group_by(lat_digits) %>% summarise(
+    me_accu = mean(lct_accu, na.rm = TRUE),
+    md_accu = median(lct_accu, na.rm = TRUE),
+    sd_accu = sd(lct_accu, na.rm = TRUE), 
+    min_accu = min(lct_accu, na.rm = TRUE),
+    max_accu = max(lct_accu, na.rm = TRUE),
+    n = n()
+  )
+
+# przy 7 cyfrach dokładność jest najwyższa
+# przy 8 średnio najniższa dokładność, ale tu jest jednen outlier
+# 9 największa rozpiętość, największa średnia i wariancja jeśli odciąć do 1000
+
+################################ wszystko było prawidłowo ####################
+
+## zapisać Rdata
+
+# write_rds(d_clean, "d_clean.rds")
+
 
 
 
