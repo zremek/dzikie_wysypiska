9d29aa66f024a189f9688e98fcda9f0ca506c798
Remigiusz Żulicki <remigiuszzulicki@gmail.com>
2026-02-17 11:00:38 +0100

    backup

--- download_photo_urls_epicollect5.R
+++ download_photo_urls_epicollect5.R
@@ -6,10 +6,10 @@ library(tidyverse)
 # step 0: manually login and download json file #####
 # from https://five.epicollect.net/project/dzikie-wysypiska/data 
 
-# 1: make df with photo urls #### TODO 2024_05_06_dzikie 
+# 1: make df with photo urls #### TODO 2026_02_17_dzikie
 
-previous_file <- "json_data/2024_05_06_dzikie.json"
-my_fresh_file <- "json_data/2024_10_01_dzikie.json" 
+previous_file <- "json_data/2024_10_01_dzikie.json"
+my_fresh_file <- "json_data/2026_02_17_dzikie.json" 
 # grudzień 337, listopad 338 obserwacji, diff 0
 
 l_fresh <- jsonlite::fromJSON(txt = my_fresh_file,
